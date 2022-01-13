314e30de52b45b74b72c64686f69eebe1ed29e69
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2022-01-12 20:24:34 -0600

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- LUALOCALS < ---------------------------------------------------------
+local include, nodecore
+    = include, nodecore
+-- LUALOCALS > ---------------------------------------------------------
+
+include("wicker")
+include("thatch")
+include("floral")
+
+include("crude")
+include("chromatic")
+
+include("carton")
+
+include("cauldron_annealed")
+include("cauldron_tempered")
+
+include("hotbox")
+
+include("ilmenite")
+include("plumbum")
+include("bamboo")
+include("thorns")
+include("adamant")
+include("battery")
+
---
+++ wicker.lua
@@ -0,0 +1,49 @@
+-- LUALOCALS < ---------------------------------------------------------
+local minetest, nodecore
+    = minetest, nodecore
+-- LUALOCALS > ---------------------------------------------------------
+
+local modname = minetest.get_current_modname()
+
+local bark = "nc_tree_tree_side.png^[mask:nc_api_storebox_frame.png"
+
+local wick = "nc_flora_wicker.png^(" .. bark .. ")"
+
+minetest.register_node(modname .. ":shelf_wicker", {
+		description = "Wicker Basket",
+--		tiles = {bark, wick},
+		tiles = {wick, wick, bark},
+		selection_box = nodecore.fixedbox(),
+		collision_box = nodecore.fixedbox(),
+		groups = {
+			snappy = 1,
+			visinv = 1,
+			flammable = 2,
+			fire_fuel = 3,
+			storebox = 1,
+			totable = 1,
+			scaling_time = 50
+		},
+		paramtype = "light",
+		sounds = nodecore.sounds("nc_tree_sticky"),
+		storebox_access = function(pt) return pt.above.y > pt.under.y end,
+		on_ignite = function(pos)
+			if minetest.get_node(pos).name == modname .. ":shelf_wicker" then
+				return nodecore.stack_get(pos)
+			end
+		end
+	})
+
+nodecore.register_craft({
+		label = "assemble wicker basket",
+		action = "stackapply",
+		indexkeys = {"nc_woodwork:form"},
+		wield = {name = "nc_flora:wicker"},
+		consumewield = 1,
+		nodes = {
+			{
+				match = {name = "nc_woodwork:form", empty = true},
+				replace = modname .. ":shelf_wicker"
+			},
+		}
+	})
