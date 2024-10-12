7c38eb340f9b3d24eda03bda5aaafb16cab9a3b3
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-12 05:43:54 -0500

    migrated rattan and wicker baskets to wc_basket

--- wicker.lua
+++ wicker.lua
@@ -4,12 +4,7 @@ local minetest, nodecore
 -- LUALOCALS > ---------------------------------------------------------
 local modname = minetest.get_current_modname()
 ------------------------------------------------------------------------
-local rfcall = function(pos, data)
-	local ref = minetest.get_player_by_name(data.pname)
-	local wield = ref:get_wielded_item()
-	wield:take_item(1)
-	ref:set_wielded_item(wield)
-end
+minetest.register_alias(modname.. ":shelf_wicker_basket",	"wc_basket:shelf_wicker")
 ------------------------------------------------------------------------
 local bark = "nc_tree_tree_side.png^[mask:nc_api_storebox_frame.png"
 local wick = "nc_flora_wicker.png^(" .. bark .. ")"
@@ -40,31 +35,6 @@ minetest.register_node(modname .. ":shelf_wicker", {
 		end
 	})
 	
-minetest.register_node(modname .. ":shelf_wicker_basket", {
-		description = "Wicker Box",
---		tiles = {bark, wick},
-		tiles = {wick, wick, bark},
-		selection_box = nodecore.fixedbox(),
-		collision_box = nodecore.fixedbox(),
-		groups = {
-			snappy = 1,
-			visinv = 1,
-			flammable = 2,
-			fire_fuel = 3,
-			storebox = 1,
-			totable = 1,
-			basketable = 1,
-			scaling_time = 50
-		},
-		paramtype = "light",
-		sounds = nodecore.sounds("nc_tree_sticky"),
-		storebox_access = function(pt) return pt.above.y > pt.under.y end,
-		on_ignite = function(pos)
-			if minetest.get_node(pos).name == modname .. ":shelf_wicker_basket" then
-				return nodecore.stack_get(pos)
-			end
-		end
-	})
 
 nodecore.register_craft({
 	label = "assemble wicker shelves",
@@ -78,16 +48,4 @@ nodecore.register_craft({
 	},
 })
 
-nodecore.register_craft({
-		label = "assemble wicker box",
-		action = "stackapply",
-		indexkeys = {modname.. ":shelf_wicker"},
-		wield = {name = "nc_flora:wicker"},
-		consumewield = 1,
-		nodes = {
-			{
-				match = {name = modname.. ":shelf_wicker", empty = true},
-				replace = modname .. ":shelf_wicker_basket"
-			},
-		}
-	})
+
